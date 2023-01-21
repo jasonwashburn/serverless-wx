74d7b1def656ee9a4816ea458d5cfffd51b6877d
Jason Washburn <jason.washburn@gmail.com>
2023-01-21 20:17:06 +0000

    adds devcontainer lifecycle scripts

---
+++ .devcontainer/onCreateCommand.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -ex
+
+sudo apt update && sudo apt install -y wget python3 python3-pip python3-eccodes
---
+++ .devcontainer/postAttachCommand.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -ex
+
+
+pip install pip-tools
+pip-sync requirements.txt dev-requirements.txt
